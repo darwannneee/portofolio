2da7c43c759a484d31e262063b870d55bb1d9d23
darwannneee <darwanchannel1@gmail.com>
2023-08-22 21:26:52 +0700

    Deleted Skill

--- src/components/home/Jumbotron.js
+++ src/components/home/Jumbotron.js
@@ -5,9 +5,13 @@ import GithubLogo from "../../assets/img/home/github.png"
 import PotoProfile from "../../assets/img/home/darwan.jpg"
 
 function Jumbotron() {
+  
   return (
     <div className='mx-8 my-6 md:mx-20 md:my-12 md:grid md:grid-cols-2'>
       <div>
+      <div class="w-max">
+    <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-2 border-r-white pr-5 text-blue-400 text-xl md:text-2xl fontTitle">BackEnd Developer</h1>
+  </div>
         <h1 className='fontJudulJumbotron text-4xl md:text-6xl'>Hi, <br />I'm Darwan</h1>
         <smal className='fontDeskripsi text-xs/loose md:text-base/loose'>Hi, my name is Darwan, I'm currently studying S1 Informatics Engineering. For now i'm living in the City of Bandung.</smal>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import Navbar from "../components/home/Navbar"
 import Jumbotron from "../components/home/Jumbotron"
-import Skill from "../components/Skill"
 
 export default function Home() {
   return (
    <div>
     <Navbar />
     <Jumbotron />
-    <Skill />
    </div>
   )
 }
