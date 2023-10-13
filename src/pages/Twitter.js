c90faf8a49a890ee6190066bfa47364c228b8da7
darwannneee <darwanchannel1@gmail.com>
2023-10-13 16:07:51 +0700

    menambahkan gambar

--- src/pages/Twitter.js
+++ src/pages/Twitter.js
@@ -1,14 +1,11 @@
-
+import imagestress from "../assets/img/home/stress.jpeg"
 export default function Twitter() {
   return (
     <div>
     {/* Konten komponen Anda di sini */}
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@username" />
-    <meta name="twitter:title" content="Judul Artikel Anda" />
-    <meta name="twitter:description" content="Deskripsi Artikel Anda" />
-    <meta name="twitter:image" content="https://darwan.me/static/media/darwan.391b82aaeebe638e5f7f.jpg" />
+    
     <h1>Hallo</h1>
+    <img src={imagestress}></img>
   </div>
   )
 }
