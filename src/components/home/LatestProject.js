7a7de9ac97f618ec91a93e8d33d5d36bb7af92f0
darwannneee <darwanchannel1@gmail.com>
2023-11-06 13:19:05 +0700

    menambahkan deskripsi di latest project

--- src/components/home/LatestProject.js
+++ src/components/home/LatestProject.js
@@ -5,7 +5,7 @@ const LatestProject = () => {
    <div className='mt-10'>
       <h1 className='text-blue-400 fontJudulJumbotron text-center text-2xl'>Latest Project</h1>
 
-     <h1 className='text-center'>Coming Soon...</h1>
+      <h1 className='text-xs/loose fontDeskripsi mt-2 text-center'>At the core of my tech Tools lies a deep understanding of Tools Development like Github.</h1>
    </div>
   )
 }
